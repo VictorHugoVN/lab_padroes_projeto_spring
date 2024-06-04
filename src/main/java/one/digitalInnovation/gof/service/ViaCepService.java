e5f97315c9d020d5a9a3f658a858d64cf46f5c7b
VictorHugoVN <victorneivadef8@gmail.com>
2024-06-04 16:06:15 -0300

    Service adicionado

---
+++ src/main/java/one/digitalInnovation/gof/service/ViaCepService.java
@@ -0,0 +1,16 @@
+package one.digitalInnovation.gof.service;
+
+import one.digitalInnovation.gof.model.Endereco;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+
+
+}
---
+++ src/main/java/one/digitalInnovation/gof/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,32 @@
+package one.digitalInnovation.gof.service.impl;
+
+import one.digitalInnovation.gof.model.Cliente;
+import one.digitalInnovation.gof.service.ClienteService;
+
+public class ClienteServiceImpl implements ClienteService {
+
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+
+    }
+
+    @Override
+    public void deletar(Long id) {
+
+    }
+}
