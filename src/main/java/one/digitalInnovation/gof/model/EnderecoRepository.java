2f5bcebcee59d7c823a5ea6a002c0416f0dd6040
VictorHugoVN <victorneivadef8@gmail.com>
2024-06-04 15:52:27 -0300

    Entidades Cliente e Endereco adicionadas

---
+++ src/main/java/one/digitalInnovation/gof/model/EnderecoRepository.java
@@ -0,0 +1,6 @@
+package one.digitalInnovation.gof.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+}
